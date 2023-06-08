1424608b75d51146fefeb23a9885966282692a2e
MJ-Avers <averroes0309@gmail.com>
2023-06-08 13:39:54 +0700

    loan

--- index2.php
+++ index2.php
@@ -1,13 +1,13 @@
 <?php
-  include "connection.php";
-  session_start();
-  $readerId = $_SESSION['reader_id'];
-  $sqlName = "select name from reader where reader_Id = $readerId";
-  $result = $db->query($sqlName);
-  if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    $name = $row["name"];
-  }
+include "connection.php";
+session_start();
+$readerId = $_SESSION['reader_id'];
+$sqlName = "select name from reader where reader_Id = $readerId";
+$result = $db->query($sqlName);
+if ($result->num_rows == 1) {
+  $row = $result->fetch_assoc();
+  $name = $row["name"];
+}
 ?>
 
 <!DOCTYPE html>
@@ -177,7 +177,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="index2.php" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SI Perpustakaan</span>
       </a>
@@ -194,13 +194,13 @@
         </div>
       </div> -->
 
-      <!-- Sidebar user panel (optional) -->
+        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
             <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block"><?php echo $name?></a>
+            <a href="#" class="d-block"><?php echo $name ?></a>
           </div>
         </div>
 
