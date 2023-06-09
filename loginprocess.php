debb0878e94aa1a2f5c6fa0ddf881172b78eaae7
MJ-Avers <averroes0309@gmail.com>
2023-06-09 08:32:03 +0700

    index

--- loginprocess.php
+++ loginprocess.php
@@ -7,16 +7,16 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $masuk = mysqli_query($db, "SELECT * FROM reader WHERE email = '$email' AND password = '$password'");
-$kontol = mysqli_num_rows($masuk);
+$cek = mysqli_num_rows($masuk);
 
-if ($kontol > 0) {
+if ($cek > 0) {
     session_start();
     $data = mysqli_fetch_assoc($masuk);
 
     // buat session member
     $_SESSION['reader_id'] = $data['reader_id'];
     $_SESSION['member_status'] = "login";
-    header("location:index2.php");
+    header("location:index.php");
 } else {
-    header("location:masuk.php?alert=gagal");
+    header("location:login.php?alert=gagal");
 }
--- viewLoanHistoryProcess.php
+++ viewLoanHistoryProcess.php
@@ -51,7 +51,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <!-- <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="index.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -184,7 +184,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index2.php" class="brand-link">
+            <a href="index.php" class="brand-link">
                 <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">SI Perpustakaan</span>
             </a>
@@ -236,7 +236,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="index2.php" class="nav-link">
+                                    <a href="index.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Loan A Book</p>
                                     </a>
@@ -248,7 +248,7 @@
                                     </a>
                                 </li>
                                 <!-- <li class="nav-item">
-                  <a href="./index3.html" class="nav-link">
+                  <a href="./index.php" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Dashboard v3</p>
                   </a>
