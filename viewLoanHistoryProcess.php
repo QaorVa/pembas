0001cfea355394a352ec7b123aa1fd1fb0e97a91
QaorVa <100664948+QaorVa@users.noreply.github.com>
2023-06-10 13:14:00 +0700

    dropdown menu

--- viewLoanHistoryProcess.php
+++ viewLoanHistoryProcess.php
@@ -184,7 +184,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index2.php" class="brand-link">
+            <a href="index.php" class="brand-link">
                 <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">SI Perpustakaan</span>
             </a>
@@ -236,7 +236,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="index2.php" class="nav-link">
+                                    <a href="index.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Loan A Book</p>
                                     </a>
