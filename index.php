7cf2e7f288a875ee549acbb050e247044aa6164e
yvlaZ <zalvyrivandy12@student.ub.ac.id>
2023-06-12 13:08:13 +0700

    klik logo

--- index.php
+++ index.php
@@ -178,7 +178,7 @@ if ($result->num_rows == 1) {
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="index2.php" class="brand-link">
+      <a href="index.php" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SI Perpustakaan</span>
       </a>
